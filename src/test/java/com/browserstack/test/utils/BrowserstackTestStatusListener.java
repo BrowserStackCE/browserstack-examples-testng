a3206bc5e08800c2b7aad2a910f9049c649baf0c
roy-de-kleijn <roy.k@browserstack.com>
2021-03-24 19:04:57 +0100

    substring error message

--- src/test/java/com/browserstack/test/utils/BrowserstackTestStatusListener.java
+++ src/test/java/com/browserstack/test/utils/BrowserstackTestStatusListener.java
@@ -8,7 +8,7 @@ import org.testng.TestListenerAdapter;
 
 public class BrowserstackTestStatusListener extends TestListenerAdapter {
 
-    private void markTestStatus(String status, String reason, WebDriver driver) { 
+    private void markTestStatus(String status, String reason, WebDriver driver) {
         try {
             JavascriptExecutor jse = (JavascriptExecutor) driver;
             jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"" + status + "\", \"reason\": \"" + reason + "\"}}");
@@ -28,7 +28,9 @@ public class BrowserstackTestStatusListener extends TestListenerAdapter {
     public void onTestFailure(ITestResult result) {
         Object currentClass = result.getInstance();
         WebDriver driver = ((TestBase) currentClass).getDriver();
-        markTestStatus("failed", result.getThrowable().getMessage(), driver);
+        String message = result.getThrowable().getMessage();
+        String reason = (message != null && message.length() > 254) ? message.substring(0, 254): message;
+        markTestStatus("failed", reason, driver);
     }
 
 }
