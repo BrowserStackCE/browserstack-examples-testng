14e8ff5471ba1a701d3be366c1de7b184d67bd9f
roy-de-kleijn <79195396+roy-de-kleijn@users.noreply.github.com>
2021-10-19 19:58:50 +0200

    New features (#15)
    * demonstrate file download feature and accessibility testing
    
    * Use TestNG SoftAssert
    
    * Removed firefox profile
    
    * removed unused imports
    
    * Filtered the accessibility errors
    
    Co-authored-by: roy-de-kleijn <roy.k@browseerstack.com>

--- src/test/java/com/browserstack/test/suites/login/AccessibilityTest.java
+++ src/test/java/com/browserstack/test/suites/login/AccessibilityTest.java
@@ -5,17 +5,21 @@ import com.deque.html.axecore.results.Results;
 import com.deque.html.axecore.results.Rule;
 import com.deque.html.axecore.selenium.AxeBuilder;
 import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AccessibilityTest extends TestBase {
 
     @Test
     public void testForAccessibility() {
+        getDriver().findElement(By.id("signin")).click();
+
         AxeBuilder builder = new AxeBuilder();
         Results results = builder.analyze(getDriver());
-        List<Rule> violations = results.getViolations();
+        List<Rule> violations = results.getViolations().stream().filter(c -> c.getId().equals("color-contrast")).collect(Collectors.toList());
 
         Assertions.assertThat(violations).isEmpty();
     }
