f5c699072458ad2b16d29466065553976f2f7067
Roy de Kleijn <roy.k@browserstack.com>
2022-04-11 14:08:23 +0200

    remove unused imports

--- src/test/java/com/browserstack/test/suites/login/IgnoreRegionVisualTest.java
+++ src/test/java/com/browserstack/test/suites/login/IgnoreRegionVisualTest.java
@@ -1,11 +1,7 @@
 package com.browserstack.test.suites.login;
 
 import com.browserstack.test.suites.TestBase;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class IgnoreRegionVisualTest extends TestBase {
