8e8491ff060c279f7c5848efa871aca403caab1f
Ansel Dsouza <ansel@browserstack.com>
2025-12-11 18:57:37 +0530

    Fix import error

--- src/test/java/com/browserstack/test/suites/ai_features/AiFeaturesTest.java
+++ src/test/java/com/browserstack/test/suites/ai_features/AiFeaturesTest.java
@@ -12,6 +12,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import com.browserstack.test.suites.TestBase;
 
 import java.net.MalformedURLException;
 import java.net.URL;
